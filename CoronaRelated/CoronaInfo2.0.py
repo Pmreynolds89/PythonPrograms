2bdbf827af73d07d1f5e21861dc7447d0fa61eca
Pmreynolds89 <54898830+Pmreynolds89@users.noreply.github.com>
2020-04-10 21:08:49 -0500

    Add files via upload
    Updated code to adapt to the site's change.
    Added more comments.

--- CoronaRelated/CoronaInfo2.0.py
+++ CoronaRelated/CoronaInfo2.0.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Corona.py 2.0b
+# Corona.py 2.0c
 from datetime import datetime
 from bs4 import BeautifulSoup
 from os import path
@@ -12,51 +12,58 @@ output = ''
 #Fetches and saves the current date to 'date'.
 date = datetime.strftime(datetime.now(), '_%Y-%m-%d')
 
+#Soup collects the html data of a site, only stores the class data for world totals, and then splits the data into a list.
 print('Retrieving data...')
 page = requests.get('https://www.worldometers.info/coronavirus/')
 soup = BeautifulSoup(page.content, 'html.parser')
 resultsTotal = soup.find(class_='total_row')
 resultstotalList = resultsTotal.text.strip().split('\n')
 
+#Formats the world totals and adds them to a string variable to save to a .txt file.
 output += 'Total'.center(31) + '\n' \
        + 'Confirmed'.ljust(31) + '-' + resultstotalList[1].rjust(10) + '\n' \
        + 'Recovered'.ljust(31) + '-' + resultstotalList[3].rjust(10) + '\n' \
        + 'Dead'.ljust(31)+ '-' + resultstotalList[5].rjust(10) + '\n\n'
 
+#Soup finds and stores the class data for country totals.
 resultsWorld = soup.find(id='main_table_countries_today')
 resultsworldFine = resultsWorld.find_all('td')
+#Counts the number of headers and saves the amount to countSumW; the headers represent different categories for each country.
 countW = resultsWorld.find_all('th')
-countSumW = 0
+countSumW = len(countW)
 
-for x in countW:
-    countSumW += 1
-    
-resultsworldList = [x.text.strip() if x.text.strip() != '' else '0' for x in resultsworldFine]
+#Strips the text and adds zeros where data is missing. Then removes irrelevant data before and after.
+resultsworldText = [x.text.strip() if x.text.strip() != '' else '0' for x in resultsworldFine]
+resultsworldList = resultsworldText[resultsworldText.index('USA'):resultsworldText.index('Total:')]
 
+#Adds headers for the upcoming country totals and adds them to the string variable.
 output += 'World'.center(31) + '\n' \
        + 'Country'.ljust(31) + 'Confirmed'.rjust(10) + 'Dead'.rjust(9) + '\n'
 
-for x in range(1, len(resultsworldList[1::countSumW])-1):
+#Formats the country totals and adds them to the string variable.
+for x in range(len(resultsworldList[::countSumW])):
     output += resultsworldList[0::countSumW][x].ljust(31) + '-' \
            + resultsworldList[1::countSumW][x].rjust(9) \
            + resultsworldList[3::countSumW][x].rjust(9) + '\n'
 
+#Soup finds and stores the id data for states in the United States.
 page = requests.get('https://www.worldometers.info/coronavirus/country/us/')
 soup = BeautifulSoup(page.content, 'html.parser')
 resultsUS = soup.find(id='usa_table_countries_today')
 resultsusFine = resultsUS.find_all('td')
+#Counts the number of headers and saves the amount to countSumU; the headers represent different categories for each state.
 countU = resultsUS.find_all('th')
-countSumU = 0
+countSumU = len(countU)
 
-for x in countU:
-    countSumU += 1
-    
+#Strips the text and adds zeros where data is missing.    
 resultsusList = [x.text.strip() if x.text.strip() != '' else '0' for x in resultsusFine]
 
+#Adds headers to the upcoming state totals and adds them to the string variable.
 output += '\n' + 'United States'.center(31) + '\n' \
        + 'State'.ljust(31) + 'Confirmed'.rjust(10) + 'Dead'.rjust(9) + '\n'
 
-for x in range(1, len(resultsusList[1::countSumU])):
+#Formats the state totals and adds them to the string variable.
+for x in range(1, len(resultsusList[::countSumU])):
     output += resultsusList[0::countSumU][x].ljust(31) + '-' \
            + resultsusList[1::countSumU][x].rjust(9) \
            + resultsusList[3::countSumU][x].rjust(9) + '\n'
@@ -66,7 +73,7 @@ pathDir = os.path.join('.','VirusData')
 if path.exists(pathDir) == False:
     os.makedirs(pathDir)
 
-#Joins and saves the filepath to a variable to make it easier to read code. 
+#Joins and saves the filepath to a variable.
 filePath = os.path.join('.', 'VirusData', 'Covid19' + date + '.txt')
 
 print('Saving data to file...')
